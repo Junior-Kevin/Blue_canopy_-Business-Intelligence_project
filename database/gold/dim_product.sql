53ccfa174a6d349f901600428503cf10238371d9
Junior <junioranalyst2021@gmail.com>
2026-08-23 12:43:34 +0300

    Enhance dim_product SQL with lag/lead and date logic
    Added lag and lead functions for product_id and modified valid_from and valid_to calculations based on flag conditions.

--- database/gold/dim_product.sql
+++ database/gold/dim_product.sql
@@ -7,6 +7,8 @@ SELECT
        product_sk  = CONVERT(INT,ROW_NUMBER() OVER (ORDER BY product_id, valid_from))
 	  ,ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY valid_from) flag
 	  ,product_id
+	  ,LEAD(product_id) OVER(PARTITION BY product_id ORDER BY product_id ) product_lag
+	  ,LAG(product_id) OVER(PARTITION BY product_id ORDER BY product_id ) product_lag1
 	  ,[product_name]
 	  ,[brand]
       ,[category]
@@ -22,7 +24,10 @@ SELECT
 					  END
       ,[introduction_date]
 	  ,[is_active] 
-      ,FORMAT([valid_from],'yyyy-MM-dd') valid_from
+      ,FORMAT(
+	        COALESCE(CAST(DATEADD(day,1,LAG(valid_to) 
+            OVER(PARTITION BY product_id ORDER BY valid_from,valid_to)) AS DATE),valid_from),
+			'yyyy-MM-dd') valid_from
       ,FORMAT(CASE WHEN [valid_to] IS NULL THEN GETDATE() 
             ELSE [valid_to] END, 'yyyy-MM-dd') valid_to
 	  ,FORMAT(CASE WHEN [discontinued_date] IS NULL THEN GETDATE() 
@@ -30,19 +35,28 @@ SELECT
 	  ,is_current_version = CASE WHEN valid_to IS NULL THEN 'yes' ELSE 'no' END
   FROM [Blue_canopy].[silver].[products]
   ) SELECT product_sk
+            --,flag
      	  ,product_id
+		 --,product_lag
+		 --,product_lag1
 	  ,[product_name]
 	  ,[brand]
       ,[category]
       ,[subcategory]
-	  ,[supplier_id]
+	  ,CASE WHEN [supplier_id] IS NULL AND flag = 1 THEN 'SUP-0018'
+	        WHEN [supplier_id] IS NULL AND flag = 2 THEN 'SUP-0060'
+			WHEN [supplier_id] IS NULL AND flag = 3 THEN 'SUP-0145'
+			WHEN [supplier_id] IS NULL AND flag = 4 THEN 'SUP-0178'
+		ELSE supplier_id END AS supplier_id
 	  ,[unit_cost_kes]
       ,[retail_price_kes]
       ,[margin_percentage]
 	  ,margin_band
 	  ,CASE WHEN flag = 1 THEN CAST('2016-01-01' AS DATE) ELSE introduction_date END AS introduction_date
 	  ,CASE WHEN flag = 1 THEN CAST('2016-01-01' AS DATE) ELSE valid_from END AS valid_from
-	  ,valid_to
+	  ,CASE WHEN flag = 1 AND product_lag IS NULL THEN CAST(GETDATE() AS DATE)
+	        WHEN product_lag IS NULL THEN CAST(GETDATE() AS DATE)
+	    ELSE valid_to END AS valid_to
 	  ,discontinued_date
 	  ,is_active
 	  ,is_current_version
