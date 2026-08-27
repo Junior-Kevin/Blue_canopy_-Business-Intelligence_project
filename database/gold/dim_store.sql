b113c1d97dc3db3b6f47c6b344cf38fc84a1928a
Junior <junioranalyst2021@gmail.com>
2026-08-27 10:06:32 +0300

    Update dim_store.sql

--- database/gold/dim_store.sql
+++ database/gold/dim_store.sql
@@ -1,20 +1,22 @@
-
 USE Blue_canopy;
 GO
 DROP TABLE IF EXISTS gold.dim_store;
 GO
-SELECT  [store_key]
-      ,[store_id]
-      ,[valid_from]
-      ,[valid_to]
-      ,[store_name]
-      ,[county]
-      ,[town]
-      ,[format]
-      ,[size_sqm]
-      ,[opening_date]
-      ,[closing_date]
-      ,[is_active]
-	  INTO gold.dim_store
-  FROM [Blue_canopy].[silver].[stores]
-
+SELECT 
+      ss.store_key AS store_sk
+     ,sb.[store_key]
+     ,ss.[store_id]
+     ,[valid_from]
+     ,[valid_to]
+     ,[store_name]
+     ,[county]
+     ,[town]
+     ,[format]
+     ,[size_sqm]
+     ,[opening_date]
+     ,[closing_date]
+     ,[is_active]
+     INTO gold.dim_store
+FROM [Blue_canopy].[silver].[stores] ss
+LEFT JOIN [Blue_canopy].gold.dim_store_bridge sb
+ON ss.store_id = sb.store_id;
