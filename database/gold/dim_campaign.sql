3c32df74075ffa0f8efec3c20df4e59fa1d8c26b
Junior <junioranalyst2021@gmail.com>
2026-09-03 22:25:05 +0300

    Update dim_campaign.sql

--- database/gold/dim_campaign.sql
+++ database/gold/dim_campaign.sql
@@ -1,19 +1,9 @@
-USE Blue_canopy;
-GO
-DROP TABLE IF EXISTS gold.dim_campaign;
-GO
-SELECT  campaign_sk  = ROW_NUMBER() OVER(ORDER BY campaign_id)
-       ,[campaign_id]
+
+DROP TABLE IF EXISTS Blue_canopy.gold.dim_campaign
+SELECT ROW_NUMBER() OVER(ORDER BY campaign_id) campaign_key
+      ,[campaign_id]
       ,[campaign_name]
       ,[campaign_type]
       ,[channel]
-      ,[start_date]
-      ,[end_date]
-	  ,[campaign_duration_days]
-      ,[budget_kes]
-      ,[actual_spend_kes]
-	  ,budget_utilisation_pct =CAST(actual_spend_kes AS FLOAT)/budget_kes*100
-      ,[discount_rate]
-	  INTO  gold.dim_campaign
+	  INTO Blue_canopy.gold.dim_campaign
   FROM [Blue_canopy].[silver].[campaigns]
-
