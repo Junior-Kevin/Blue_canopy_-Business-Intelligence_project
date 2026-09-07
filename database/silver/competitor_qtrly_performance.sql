dcae01b96c0df5665590505a644f08ed2406b976
Junior <junioranalyst2021@gmail.com>
2026-09-07 23:11:06 +0300

    Modify competitor quarterly performance SQL query
    Added store_name and county_key columns, and corrected revenue_kes assignment.

--- database/silver/competitor_qtrly_performance.sql
+++ database/silver/competitor_qtrly_performance.sql
@@ -3,7 +3,9 @@ DROP TABLE IF EXISTS silver.competitor_quarterly
 GO
 SELECT comp_qtr_key = ROW_NUMBER() OVER(ORDER BY [quarter])
        ,[competitor_id]
-      ,[quarter]
+	  , store_name= [quarter]
+	  ,ABS(CHECKSUM(NEWID()) % 47) + 1 AS county_key
+      ,[quarter] =[revenue_kes]
 	  ,CAST(SUBSTRING(market_share_pct,1,CHARINDEX(',',[market_share_pct])-1) AS INT) revenue_kes
 	  ,CAST(SUBSTRING(market_share_pct,CHARINDEX(',',[market_share_pct])+1,4) AS FLOAT) market_share_pct
 INTO silver.competitor_quarterly
