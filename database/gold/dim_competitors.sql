476bff14a98d76c677d900cfb229e76c91bf3c9b
Junior <junioranalyst2021@gmail.com>
2026-09-09 02:59:41 +0300

    Update dim_competitors.sql to use silver data source
    Refactored SQL to select from silver competitors table instead of bronze competitor stores.

--- database/gold/dim_competitors.sql
+++ database/gold/dim_competitors.sql
@@ -1,13 +1,9 @@
+
 DROP TABLE IF EXISTS Blue_canopy.gold.dim_competitors;
 GO
-SELECT ROW_NUMBER() OVER(ORDER BY SR.[competitor_id]) competitor_key
-      ,[competitor_store_id]
-      ,SR.[competitor_id]
-	  ,competitor_name
-      ,[location]
-      ,[county]
-      ,[size_category]
-	  INTO Blue_canopy.gold.dim_competitors
-FROM [Blue_canopy].[bronze].[competitor_stores_raw] SR
-LEFT JOIN [Blue_canopy].[bronze].[competitors_raw] CR
-ON SR.competitor_id  = CR.competitor_id
+SELECT  
+ROW_NUMBER() OVER(ORDER BY[competitor_id]) competitor_key
+      ,[competitor_id]
+      ,[competitor_name]
+  INTO Blue_canopy.gold.dim_competitors
+  FROM [Blue_canopy].[silver].[competitors]
