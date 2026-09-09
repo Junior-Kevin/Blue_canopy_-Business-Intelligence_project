b70133e9d027ad985b31f7a1339c380a9a8d37d6
Junior <junioranalyst2021@gmail.com>
2026-09-09 03:12:40 +0300

    Remove county_key calculation from SQL query
    Removed the calculation for county_key from the SELECT statement.

--- database/silver/competitor_qtrly_performance.sql
+++ database/silver/competitor_qtrly_performance.sql
@@ -4,7 +4,6 @@ GO
 SELECT comp_qtr_key = ROW_NUMBER() OVER(ORDER BY [quarter])
        ,[competitor_id]
 	  , store_name= [quarter]
-	  ,ABS(CHECKSUM(NEWID()) % 47) + 1 AS county_key
       ,[quarter] =[revenue_kes]
 	  ,CAST(SUBSTRING(market_share_pct,1,CHARINDEX(',',[market_share_pct])-1) AS INT) revenue_kes
 	  ,CAST(SUBSTRING(market_share_pct,CHARINDEX(',',[market_share_pct])+1,4) AS FLOAT) market_share_pct
